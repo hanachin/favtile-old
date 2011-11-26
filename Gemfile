c570fe365567970f21b07d0631ae2a4f0c658610
hanachin <hanachin@gmail.com>
2011-11-26 17:45:07 +0900

    show tweets

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 gem 'sinatra'
 gem 'thin'
+gem 'haml'
+gem 'twitter'
--- web.rb
+++ web.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'twitter'
 
-get '/' do
-  "Hello, world"
+Twitter.configure do |config|
+  config.consumer_key = "your consumer_key"
+  config.consumer_secret = "your consumer_secret"
+  config.oauth_token = "your oauth_token"
+  config.oauth_token_secret = "your oauth_token_secret"
+end
+
+get '/:name' do |name|
+  @favs = Twitter.favorites(name).map {|t| t.text }
+  haml :index
 end
